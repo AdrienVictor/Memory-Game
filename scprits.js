a56bf7cc57991b371981d0b5038435d288e0ffcd
AdrienVictor <46904762+AdrienVictor@users.noreply.github.com>
2019-02-28 16:43:46 +0200

    creating a memory game

---
+++ scprits.js
@@ -0,0 +1,48 @@
+const cards = document.querySelectorAll(".memory-card");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.toggle("flip");
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  checkForMatch();
+}
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1000);
+}
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+// this is a  IIFE (immediatly invoked function expression)
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 16);
+    card.style.order = randomPosition;
+  });
+})();
+cards.forEach(card => card.addEventListener("click", flipCard));
